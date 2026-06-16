01fe25ad43b8ba4279171c4881be8cf378195111
Juana Molina <juanamolina854@outlook.com>
2026-06-16 00:54:12 -0300

    cambios en index y main

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import PrimeVue from 'primevue/config'
-import Aura from '@primeuix/themes/aura'
-
 import App from './App.vue'
 import router from './router'
 
-import 'primeicons/primeicons.css'
+import '@/assets/styles/forms.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
-
 app.use(router)
-
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
 
 app.mount('#app')
