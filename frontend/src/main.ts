6368f747a509f740d4d123f11999ba96a610d30f
nicorsss <nicoreisnl@gmail.com>
2026-06-18 03:43:52 -0300

    persistencia al cerrar sesion

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,12 +3,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from '@/stores/auth'
 
 import '@/assets/styles/forms.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
+
+const authStore = useAuthStore()
+authStore.initAuth()
+
 app.use(router)
 
 app.mount('#app')
